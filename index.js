15b76c99f782a753528340ea338c016429eab6dc
codedbystuart <code.stuartdambi@gmail.com>
2021-05-04 23:18:28 +0300

    [ADD added products route wrapper to main app file

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
+const ProductsRoute = require('./routes/ProductRoute');
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/api/v1/products', ProductsRoute);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => { });
