fa814d2eada5acad0fcd26edacf971b1cbab8dd0
byasi <solomonbyasi@gmail.com>
2021-05-07 03:51:03 +0300

    Created a Login returning a token for the Logged in User

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -69,6 +69,39 @@ static async singleUser(req,res,next){
     return next(error);
   }
 }
+static async login(req, res, next){
+  try {
+    const userToLogin = {
+      email: req.body.email,
+      password: req.body.password
+    }
+    const ifUserExists = await UserServices.findUserbyEmail(userToLogin.email);
+    if(ifUserExists){
+      const token  = await jwt.sign({ifUserExists},process.env.JWT_SECRETEKEY);
+      const unhashPass = await bcrypt.compare(userToLogin.password,ifUserExists.password);
+      if(unhashPass){
+        return res.status(200).json({
+          status: res.statusCode,
+          message: 'Login successful',
+          token
+        });
+      } else {
+        return res.status(409).json({
+          status: res.statusCode,
+          message: 'Wrong Email or Password'
+        });
+      }
+      
+    } else {
+      return res.status(409).json({
+        status: res.statusCode,
+        message: 'Wrong Email or Password '
+      });
+    }
+  } catch (error) {
+    return next(error);
+  }
+}
 
 }
 
