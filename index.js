30c1ae9ef81743090763c239de945a48a281b404
byasi <solomonbyasi@gmail.com>
2021-05-14 11:33:45 +0300

    included a rawMaterial router page

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 // Router Pages
 const userRoute = require('./routes/UserRoute');
+const Rawmaterials = require('./routes/RawmaterialRoute');
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1/auth',userRoute);
+app.use('/api/v1',Rawmaterials);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {console.log(`server is running at http://localhost:${port}`) });
