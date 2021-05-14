50c9a7b0db131b41deee4c626087dee683d07b3e
byasi <solomonbyasi@gmail.com>
2021-05-14 13:22:02 +0300

    add a get Route to get all rawmaterials

--- routes/RawmaterialRoute.js
+++ routes/RawmaterialRoute.js
@@ -4,6 +4,7 @@ const rawMaterialController = require('../controllers/RawmaterialController');
 const router = express.Router();
 
 router.post('/addmaterial',rawMaterialController.addRawmaterial);
+router.get('/rawmaterials', rawMaterialController.getRawmaterials);
 
 
 module.exports = router;
