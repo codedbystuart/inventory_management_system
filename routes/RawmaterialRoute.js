4d204dbf301207dbd6c7030f6fe2a66bda6c3c0e
byasi <solomonbyasi@gmail.com>
2021-05-14 14:32:27 +0300

    added a delete route

--- routes/RawmaterialRoute.js
+++ routes/RawmaterialRoute.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.post('/addmaterial',rawMaterialController.addRawmaterial);
 router.get('/rawmaterials', rawMaterialController.getRawmaterials);
 router.get('/rawmaterials/:id',rawMaterialController.getSingleRawmaterial);
+router.delete('/deletematerial/:id',rawMaterialController.deleteRawMaterial);
 
 
 module.exports = router;
