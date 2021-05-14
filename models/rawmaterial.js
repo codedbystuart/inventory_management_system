c090c01c2f3ae8913ae5178fbfd80c023ad0518f
byasi <solomonbyasi@gmail.com>
2021-05-14 11:04:45 +0300

    created a raw materials table with attributes name and cost

---
+++ models/rawmaterial.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Rawmaterial extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Rawmaterial.init({
+    name: DataTypes.STRING,
+    cost: DataTypes.FLOAT
+  }, {
+    sequelize,
+    modelName: 'Rawmaterial',
+  });
+  return Rawmaterial;
+};
