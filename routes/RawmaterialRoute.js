57d797b91e9f34cc64ec22139a182c24a94f6992
byasi <solomonbyasi@gmail.com>
2021-05-14 13:32:18 +0300

    added a get route to get a single material in the table by id

--- routes/RawmaterialRoute.js
+++ routes/RawmaterialRoute.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post('/addmaterial',rawMaterialController.addRawmaterial);
 router.get('/rawmaterials', rawMaterialController.getRawmaterials);
+router.get('/rawmaterials/:id',rawMaterialController.getSingleRawmaterial);
 
 
 module.exports = router;
