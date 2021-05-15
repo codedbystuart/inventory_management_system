0b3851fed53a4162730532333a07d8aa36c6a95c
byasi <solomonbyasi@gmail.com>
2021-05-15 14:50:30 +0300

    added a get api to fetch all products

--- routes/ProductsRoute.js
+++ routes/ProductsRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const productsController = require('../controllers/ProductsController');
+const { route } = require('./UserRoute');
 
 const router = express.Router();
 router.post('/addproduct',productsController.addProduct);
+router.get('/products',productsController.getProducts);
 
 
 
