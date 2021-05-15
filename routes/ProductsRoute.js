ea09ab0d4b6de5ce1becbe6e344d9423337f6f7b
byasi <solomonbyasi@gmail.com>
2021-05-16 00:03:31 +0300

    added a get api to fetch a single product in the table

--- routes/ProductsRoute.js
+++ routes/ProductsRoute.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const productsController = require('../controllers/ProductsController');
-const { route } = require('./UserRoute');
 
 const router = express.Router();
 router.post('/addproduct',productsController.addProduct);
 router.get('/products',productsController.getProducts);
-
-
+router.get('/products/:id',productsController.getSingleProduct);
 
 module.exports = router;
